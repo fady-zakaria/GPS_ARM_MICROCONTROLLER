8503da10f5cda60171dbc7095d0c0f5f5914d43d
fady-zakaria <fady.zakaria93@gmail.com>
2021-06-12 11:26:07 +0200

    UPDATED UART-CONNECTION-with-PC

--- uart_with_pc.c
+++ uart_with_pc.c
@@ -96,6 +96,92 @@ void Data_Transmit_Float(float no)
 
 //*******************************************************************************************
 
+//function to transmit string to check the UART7
+void	transmit_string(char * str)
+{
+	while(*str)
+	{
+		UART7_Transmitter(*(str++));
+	}
+}
+
+//*******************************************************************************************
+
+//*************************************** UART0 (if UART7 didn't work) ***********************************************
+/*
+
+void UART0_Transmitter(char data)
+{
+	while((UART0_FR_R & 0x0020) != 0);
+	UART0_DR_R = data;
+} 
+
+char UART0_Receiver(void)
+{
+	while((UART0_FR_R & 0x0010) != 0);	
+	return (char)(UART0_DR_R & 0xFF);
+}
+
+void UART0_Init(void)
+{
+	SYSCTL_RCGCUART_R |= (1<<0); // UART 0 --> 0000 0001
+	SYSCTL_RCGCGPIO_R |= (1<<0); // Port A (0000 0001) ( Pin 0 (PA0) --> U0Rx , Pin 1 (PA1) --> U0Tx )
+	SysTick_Wait1ms(100);
+	
+	//PORT A
+	GPIO_PORTA_AFSEL_R |= 0x03; //enable alt function to PA0 ,PA1
+	GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R & 0xFFFFFF00)+0x00000011;
+	GPIO_PORTA_DEN_R |= 0x03; //enable digital
+	GPIO_PORTA_AMSEL_R &= ~0x03; //disable analog
+
+	UART0_CTL_R &= ~0x01; //disable UART0 module (Reset)
+	//set the buad rate
+	UART0_IBRD_R = 104;
+	UART0_FBRD_R = 11;
+	UART0_LCRH_R = 0x0070; // No parity - FIFO enabled - data length = 8 bits - one stop bit
+	UART0_CC_R = 0x0; // to enable system clock to uart0
+	UART0_CTL_R = 0x301; // enable RXE,TXE and UART
+
+}
+
+// *******************************************************************************************
+//function to change from float to char to send it to UART0
+void Data_Transmit_Float(float no)
+{
+//  char toprint[10];
+//  sprintf(toprint, "%f", no);
+//  itoa(no, toprint);
+  
+  int len = snprintf('\0', 0, "%f", no);
+  char* transmit_no = (char *)malloc(len + 1);
+  snprintf(transmit_no, len + 1, "%f", no);
+  int j = 0;
+  while (j < len - 3 )
+  {
+    UART0_Transmitter(transmit_no[j]);
+    j++;
+  }
+  free(transmit_no);
+}
+
+// *******************************************************************************************
+
+//function to transmit string to check the UART0
+void	transmit_string(char * str)
+{
+	while(*str)
+	{
+		UART0_Transmitter(*(str++));
+	}
+}
+
+// *******************************************************************************************
+
+*/
+
+//*******************************************************************************************
+
+
 int main()
 {
 	float Coordinates_points[30];
@@ -104,9 +190,13 @@ int main()
 	
 	SysTick_Init();
 	UART7_Init();
-		
+	
+	//UART0_Init();
+	
+	
 	while(1)
 	{
+		transmit_string("Enter :\n\r"); // To check the Connection
 		check = UART7_Receiver();
 		if(check=='U'){
 			for(i=0;i<30;i++)
